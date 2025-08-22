a9b6cc197d1ea0f81cce34345f1e6e5f5604faca
JayBradCoding <jbradl24@villanova.edu>
2025-08-22 02:33:26 -0400

    updated CORS for deployed frontend in main

--- backend/app/main.py
+++ backend/app/main.py
@@ -24,7 +24,10 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=[
+        "https://use-autoaudit.vercel.app",
+        "http://localhost:5173"
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
